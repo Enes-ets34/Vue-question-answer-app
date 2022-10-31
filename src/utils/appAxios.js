d80378ce80914fc0452fc990aed1badf3b478732
Enes Taha Sarı <50265845+Enes-ets34@users.noreply.github.com>
2022-10-31 23:02:01 +0300

    Update appAxios.js

--- src/utils/appAxios.js
+++ src/utils/appAxios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const appAxios = axios.create({
-  // baseURL: "http://localhost:3000",
-   baseURL: "https://my-json-server.typicode.com/Enes-ets34/qa-app-json-server",
+   baseURL: "http://localhost:3000",
+  // baseURL: "https://my-json-server.typicode.com/Enes-ets34/qa-app-json-server",
   withCredentials: false,
   headers: {
     "Content-Type": "application/json"
